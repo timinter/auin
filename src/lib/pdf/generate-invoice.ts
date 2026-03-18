9afd2702e7a8b7f5b397afed9d79dc8da83af7a3
timinter <timofey.bykov@interexy.com>
2026-03-18 15:46:29 +0300

    fix build issues

--- src/lib/pdf/generate-invoice.ts
+++ src/lib/pdf/generate-invoice.ts
@@ -3,9 +3,8 @@ import { renderToBuffer } from "@react-pdf/renderer";
 import { InvoiceDocument, InvoiceData } from "./invoice-template";
 
 export async function generateInvoicePdf(data: InvoiceData): Promise<Buffer> {
-  const buffer = await renderToBuffer(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    React.createElement(InvoiceDocument, { data }) as any
-  );
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const element = React.createElement(InvoiceDocument, { data }) as any;
+  const buffer = await renderToBuffer(element);
   return Buffer.from(buffer);
 }
